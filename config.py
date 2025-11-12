c1fb596004b8e91bdb095e79704aa5a084bcbcd4
Richthofens <na252720@gmail.com>
2025-11-12 20:35:28 -0300

    upd

--- config.py
+++ config.py
@@ -1,9 +1,10 @@
 import MySQLdb
 
 db = MySQLdb.connect(
-    host='localhost',
-    user='root',
-    passwd='1234'
-    db='controle_epi'
-    ''
+    host="localhost",
+    user="seu_usuario",        # Ex: 'root'
+    passwd="sua_senha",        # Senha do MySQL
+    db="controle_epi"          # Nome do banco de dados
 )
+
+cursos = db.cursor()

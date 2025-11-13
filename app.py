41a948408250303dac886862060f7e0efc94bd25
Richthofens <na252720@gmail.com>
2025-11-12 21:02:21 -0300

    upd

--- app.py
+++ app.py
@@ -1,11 +1,11 @@
-from flask import Flask, request, abort, render_template
+from flask import Flask, request, abort, render_template, url_for
 import pandas
 
 app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return render_template('index.html')
+    return render_template('epi360.html')
 
 
 if __name__ == '__main__':
